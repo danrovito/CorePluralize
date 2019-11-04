0142bcdb897cfaaf8c1b7efe97cbc85d9624a6ec
Dan Rovito <drovito@thinkwarenet.com>
2019-11-04 11:00:32 -0500

    updated folder structure

---
+++ CorePluralize.Tests/PluralizerTests.cs
@@ -0,0 +1,54 @@
+using CorePluralize.Tests;
+using CorePluralize;
+using System;
+using Xunit;
+
+namespace CorePluralize.Tests
+{
+	public class PluralizerTests
+    {
+		Pluralizer _pluralizer = new Pluralizer();
+
+        [Fact]
+		public void InputData()
+        {
+			var input = Resources.InputData.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in input)
+            {
+                var singular = line.Split(',')[0];
+                var plural = line.Split(',')[1];
+                Assert.Equal(plural, _pluralizer.Pluralize(singular));
+				Assert.Equal(plural, _pluralizer.Pluralize(plural));
+				Assert.Equal(singular, _pluralizer.Singularize(plural));
+				Assert.Equal(singular, _pluralizer.Singularize(singular));
+            }
+
+        }
+
+		[Fact]
+		public void ExceptionPluralToSingularException()
+        {
+			var input = Resources.PluralToSingularExceptions.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in input)
+            {
+                var singular = line.Split(',')[0];
+                var plural = line.Split(',')[1];
+				Assert.Equal(singular, _pluralizer.Singularize(plural));
+				Assert.Equal(singular, _pluralizer.Singularize(singular));
+            }
+        }
+
+		[Fact]
+		public void ExceptionSingularToPluralException()
+        {
+			var input = Resources.SingularToPluralExceptions.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in input)
+            {
+                var singular = line.Split(',')[0];
+                var plural = line.Split(',')[1];
+				Assert.Equal(plural, _pluralizer.Pluralize(singular));
+				Assert.Equal(plural, _pluralizer.Pluralize(plural));
+            }
+        }
+    }
+}
